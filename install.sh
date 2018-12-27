9952b8e31578a5475f6f47c341c37a1d9c44afc1
Luiz Felipe <23662020+lffg@users.noreply.github.com>
2018-12-27 19:50:08 -0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,31 @@
-yarn add tslint-config-prettier --dev
+URI="https://raw.githubusercontent.com/lffg/ts/master"
 
-# If the project uses React:
-# yarn add tslint-config-prettier tslint-react --dev
+FILES=(
+  "tsconfig.json"
+  "tslint.json"
+)
+
+for file in "${FILES[@]}"; do
+  if [ -f "./$file" ]; then
+    echo "Removing the \`$file\` file..."
+    rm "./$file"
+  fi
+
+  echo "Downloading the \`$file\` file..."
+  curl -s -X GET "$URI/$file" -o $file
+done
+
+echo "\nDone."
+echo "\nDo you want to install the dependencies (will use Yarn)? (y/N)"
+read install
+
+if [ "$response" = "y" -o "$response" = "Y" ]; then
+  if [ ! -f "package.json" ]; then
+    yarn init -y --silent
+  fi
+
+  yarn add typescript tslint tslint-config-prettier --silent --dev
+  echo "\nDone."
+else
+  echo "Ok. Bye."
+fi
